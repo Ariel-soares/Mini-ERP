916f3744f60a2e789c7d17dcc6b6e18aa67fe416
Ariel Soares <ariel.sfranco@gmail.com>
2024-03-24 21:05:01 -0300

    products entity refined

--- src/main/java/com/arielSoares/entities/Product.java
+++ src/main/java/com/arielSoares/entities/Product.java
@@ -1,7 +1,11 @@
 package com.arielSoares.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="tb_product")
 public class Product {
 
     @Id
