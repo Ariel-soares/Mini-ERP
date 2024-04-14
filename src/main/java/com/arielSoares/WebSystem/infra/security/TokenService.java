56ba29f7b22b9f1b8589f902d9d39992fba68761
Ariel Soares <ariel.sfranco@gmail.com>
2024-04-13 21:34:42 -0300

    Token Verification method created

--- src/main/java/com/arielSoares/WebSystem/infra/security/TokenService.java
+++ src/main/java/com/arielSoares/WebSystem/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.arielSoares.WebSystem.entities.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +30,19 @@ public class TokenService {
             return token;
         }catch (JWTCreationException exception){
             throw new RuntimeException("Could not generate token", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch(JWTVerificationException exception){
+            return "";
         }
     }
 
