d83f58196b844ea06930b943381dffa67fec0354
Ariel Soares <ariel.sfranco@gmail.com>
2025-01-13 19:40:14 -0300

    Minor changes

--- create.sql
+++ create.sql
@@ -1,3 +1,1 @@
 create table tb_user (id bigserial not null, email varchar(255), is_admin boolean, name varchar(255), password varchar(255), username varchar(255), primary key (id));
-create table tb_user (id bigserial not null, email varchar(255), is_admin boolean, name varchar(255), password varchar(255), username varchar(255), primary key (id));
-create table tb_user (id bigserial not null, email varchar(255), is_admin boolean, name varchar(255), password varchar(255), username varchar(255), primary key (id));
